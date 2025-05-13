e32a18e137d0b7060f70a899cea31737293ebad4
Abarchi <Oups@MISTER-BARKE.local>
2025-05-14 00:40:45 +0100

    first commit

--- find_string.sh
+++ find_string.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Demander le nom du fichier
-echo -n "Entrez le nom du fichier:"
 read f
-# Demander la chaîne de caractères à rechercher
-echo -n "Entrez la chaîne de caractères à rechercher:"
 read c
 # Vérifier si la chaîne existe dans le fichier
 if grep -q "$c" "$f"; then
