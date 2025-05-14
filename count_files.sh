895f3858d67a96e2e4c1baaf2d2931bda20c417f
Abarchi <Oups@MISTER-BARKE.local>
2025-05-14 16:15:56 +0100

    azerty

--- count_files.sh
+++ count_files.sh
@@ -2,8 +2,7 @@
 
 # Demander à l'utilisateur d'entrer le chemin d'un répertoire
 read a 
-
 # Vérifier que le chemin entré est bien un répertoire
 n="$(ls "$a" | wc -l)"
 b=$(echo $n)
-echo "Le répertoire "$a" contient "$b" fichier(s)."
+echo "Le répertoire $a contient $b fichier(s)."
--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-a=$1
-b=$2
-op=$3
+
 read a b op
 
 if [ "$op" = "+" ]; then
