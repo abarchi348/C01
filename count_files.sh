fe779cb0136a48995cfc39534acfa2ec07bd64bf
Abarchi <Oups@MISTER-BARKE.local>
2025-05-14 13:38:17 +0100

    first commit

--- count_files.sh
+++ count_files.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+
 # Demander à l'utilisateur d'entrer le chemin d'un répertoire
-read a
-# Compter les fichiers (exclut les dossiers)
+read -p "Entrez le chemin du répertoire :" a
+
+# Vérifier que le chemin entré est bien un répertoire
+if [ -d "$a" ]; then
+  # Compter les fichiers (exclut les dossiers)
   n=$(ls -p "$a" | grep -v / | wc -l)
   echo "Le répertoire '$a' contient $n fichier(s)."
+else
+  echo "Erreur : '$a' n'est pas un répertoire valide."
+fi
+
 
