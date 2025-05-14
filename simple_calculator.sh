38700cc4e35d5b59578057fe58af3dab1eb4e37f
Abarchi <Oups@MISTER-BARKE.local>
2025-05-14 15:56:39 +0100

    azzerty

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,47 +1,20 @@
 #!/bin/bash
 
-# Fonction pour la somme
-somme() {
-  echo $(( $1 + $2 ))
-}
+read a b op
 
-# Fonction pour la différence
-difference() {
-  echo $(( $1 - $2 ))
-}
-
-# Fonction pour la multiplication
-multiplication() {
-  echo $(( $1 * $2 ))
-}
-
-# Fonction pour la division
-division() {
-  if [ "$2" -eq 0 ]; then
-    echo "Erreur : division par zéro"
-  else
-    echo $(( $1 / $2 ))
-  fi
-}
-
-# Lecture des entrées sur une seule ligne
-read -p "Entrez deux nombres et un opérateur (ex: 5 3 +) : " a b op
-
-# Appel de la bonne fonction selon l'opérateur
-case "$op" in
-  +)
-    echo "Résultat : $(somme $a $b)"
-    ;;
-  -)
-    echo "Résultat : $(difference $a $b)"
-    ;;
-  \*)
-    echo "Résultat : $(multiplication $a $b)"
-    ;;
-  /)
-    echo "Résultat : $(division $a $b)"
-    ;;
-  *)
-    echo "Erreur : opérateur invalide. Utilisez +, -, *, ou /"
-    ;;
-esac
+if [ "$op" = "+" ]; then
+    resultat=$(($a + $b))
+elif [ "$op" = "-" ]; then
+    resultat=$(($a - $b))
+elif [ "$op" = "*" ]; then
+    resultat=$(($a * $b))
+elif [ "$op" = "/" ]; then
+    if [ "$b" -eq 0 ]; then
+        echo "Erreur : division par zéro"
+        exit 1
+    else
+        resultat=$(($a / $b))
+    fi
+fi
+echo "Résultat : $resultat"
+exit 0
