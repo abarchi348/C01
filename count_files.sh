f0a98ddfc816aa2e60b76e2339fc50ca93a5f7f6
Abarchi <Oups@MISTER-BARKE.local>
2025-05-14 12:44:49 +0100

    first commit

--- count_files.sh
+++ count_files.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+# Demander à l'utilisateur d'entrer le chemin d'un répertoire
 read a
-# Compter les fichiers avec ls et wc -l
-n=$(ls -p "$a" | grep -v / | wc -l)
-echo "Le répertoire '$a' contient $n fichier(s)."
+
+# Vérifier que le répertoire existe
+if [ -d "$a" ]; then
+  # Compter les fichiers (exclut les dossiers)
+  n=$(ls -p "$a" | grep -v / | wc -l)
+  echo "Le dossier '$a' contient $n fichier(s)."
+else
+  echo "Erreur : '$a' n'est pas un dossier valide."
+fi
+
--- find_string.sh
+++ find_string.sh
@@ -6,5 +6,6 @@ read c
 if grep -q "$c" "$f"; then
     echo "La chaîne '$c' a été trouvée dans '$f'."
 else
-    echo "La chaîne '$c' n'a pas été trouvée dans '$f'."
+    echo "La chaîne '$c' n'a pas été trouvée dans 
+    '$f'."
 fi
