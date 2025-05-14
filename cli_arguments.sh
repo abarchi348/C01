00b4fd44627bf4b06c366e029155e9c10fc8a5aa
Abarchi <Oups@MISTER-BARKE.local>
2025-05-14 12:54:08 +0100

    first commit

---
+++ cli_arguments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -a arg
+set -- "${arg[@]}"
+# Affichage des arguments avec $1, $2, ... et shift
+index=1
+while [ "$1" ]; do
+    echo "Argument $index: $1"
+    shift
+    index=$((index + 1))
+done
