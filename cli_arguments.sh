2b8cf877edc9ba550a5abc44726c2aefe2ad56df
Abarchi <Oups@MISTER-BARKE.local>
2025-05-14 15:12:15 +0100

    first commit

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -4,7 +4,7 @@ set -- "${arg[@]}"
 # Affichage des arguments avec $1, $2, ... et shift
 index=1
 while [ "$1" ]; do
-    echo "Argument $index: $1"
+    echo "Argument $index : $1"
     shift
     index=$((index + 1))
 done
--- count_files.sh
+++ count_files.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 # Demander à l'utilisateur d'entrer le chemin d'un répertoire
-read -p "Entrez le chemin du répertoire :" a
+read a 
 
 # Vérifier que le chemin entré est bien un répertoire
-if [ -d "$a" ]; then
-  # Compter les fichiers (exclut les dossiers)
-  n=$(ls -p "$a" | grep -v / | wc -l)
-  echo "Le répertoire '$a' contient $n fichier(s)."
-else
-  echo "Erreur : '$a' n'est pas un répertoire valide."
-fi
-
-
+n="$(ls "$a" | wc -l)"
+b=$(echo $n)
+echo "Le répertoire "$a" contient "$b" fichier(s)."
