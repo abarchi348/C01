8e36f60274c6bc6a815f3b64c3f8a4a952c9d790
Abarchi <Oups@MISTER-BARKE.local>
2025-05-14 16:08:42 +0100

    azerty

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -9,12 +9,7 @@ elif [ "$op" = "-" ]; then
 elif [ "$op" = "*" ]; then
     resultat=$(($a * $b))
 elif [ "$op" = "/" ]; then
-    if [ "$b" -eq 0 ]; then
-        echo "Erreur : division par zéro"
-        exit 1
-    else
-        resultat=$(($a / $b))
-    fi
+    resultat=$(($a / $b))
 fi
 echo "Résultat : $resultat"
 exit 0
